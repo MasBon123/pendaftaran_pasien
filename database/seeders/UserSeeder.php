d990898015d8e19a95bcd99f37c06c0edba76afd
MasBon123 <sampingoli987@gmail.com>
2021-12-23 04:10:32 -0500

    Role

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace Database\Seeders;
-use App\Models\user;
+use App\Models\User;
+use App\Models\Role;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
@@ -14,34 +15,33 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        $user = new User();
-        $user->name = 'Admin pasien';
-        $user->email = 'Admin@gmail.com';
-        $user->password = Hash::make('12345678');
-        $user->save();
+       //membuat sample role
+       $adminRole = new Role();
+       $adminRole->name = "admin";
+       $adminRole->display_name = "Administrator";
+       $adminRole->save();
 
-        $user = new User();
-        $user->name = 'jojo';
-        $user->email = 'jojo@gmail.com';
-        $user->password = Hash::make('12345678');
-        $user->save();
+       $memberRole = new Role();
+       $memberRole->name = "member";
+       $memberRole->display_name = "Member";
+       $memberRole->save();
 
-        $user = new User();
-        $user->name = 'klarisa';
-        $user->email = 'klarisa@gmail.com';
-        $user->password = Hash::make('12345678');
-        $user->save();
+       //membuat sample admin
+       $admin = new User();
+       $admin->name = 'Admin Pasien';
+       $admin->email = 'admin@gmail.com';
+       $admin->password = Hash::make('12345678');
+       $admin->save();
+       $admin->attachRole($adminRole);
 
-        $user = new User();
-        $user->name = 'amelia';
-        $user->email = 'amelia@gmail.com';
-        $user->password = Hash::make('12345678');
-        $user->save();
+       //membuat sample member
+       $member = new User();
+       $member->name = "Member pasien";
+       $member->email = "member@gmail.com";
+       $member->password = Hash::make('12345678');
+       $member->save();
+       $member->attachRole($memberRole);
 
-        $user = new User();
-        $user->name = 'adinda';
-        $user->email = 'adinda@gmail.com';
-        $user->password = Hash::make('12345678');
-        $user->save();
+
     }
 }
