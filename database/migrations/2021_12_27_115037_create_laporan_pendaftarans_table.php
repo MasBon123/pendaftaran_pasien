474dc1540dbb9677e6397a4dd54e3dda523214a4
MasBon123 <sampingoli987@gmail.com>
2021-12-28 04:39:33 -0500

    db

---
+++ database/migrations/2021_12_27_115037_create_laporan_pendaftarans_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLaporanPendaftaransTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('laporan_pendaftarans', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_pasien');
+            $table->string('keluhan');
+            $table->date('tanggal_daftar');
+            $table->integer('no_telepon');
+            $table->string('jk');
+            $table->string('nama_dokter');
+            $table->date('jadwal_periksa');
+            $table->string('ruang');
+            $table->string('cara_bayar');
+            $table->timestamps();
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('laporan_pendaftarans');
+    }
+}
